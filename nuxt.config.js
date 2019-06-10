b0ed60cedb113a964f7ef87f9d1734bec996dc5d
Alistair Shepherd <alistair@accudio.com>
2019-06-10 23:09:25 +0100

    Added favicon and open-graph image

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,22 +8,29 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'J.K. Rowling Bot - Harry Potter tweets with machine learning',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Have you ever wondered what happens when you run all of J.K. Rowling\'s \'Harry Potter\' plot related tweets through a neural network and ask it to create some more? Well I did, and this is the result!' }
     ],
     link: [
-      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#80a2bc' },
-      { name: 'msapplication-TileColor', content: '#2d89ef' },
-      { name: 'msapplication-config', content: '/browserconfig.xml' },
-      { name: 'theme-color', content: '#5e809a' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,700,900&display=swap' }
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#3b94d9' },
+      { name: 'apple-mobile-web-app-title', content: 'J.K. Rowling Bot' },
+      { name: 'application-name', content: 'J.K. Rowling Bot' },
+      { name: 'msapplication-TileColor', content: '#3b94d9' },
+      { name: 'theme-color', content: '#3b94d9' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,700,900&display=swap' },
+      { property: 'og:image:width', content: '1190' },
+      { property: 'og:image:height', content: '623' },
+      { property: 'og:description', content: 'Ever wondered what happens when you run J.K. Rowling\'s \'Harry Potter\' plot related tweets through a neural network and make it create more? This is the result!' },
+      { property: 'og:title', content: 'J.K. Rowling Bot - Harry Potter tweets with machine learning' },
+      { property: 'og:url', content: 'https://jkrowling.alistairshepherd.uk' },
+      { property: 'og:image', content: 'https://jkrowling.alistairshepherd.uk/og-image.jpg' }
     ]
   },
 
@@ -60,8 +67,8 @@ export default {
     name: 'JK Rowling Bot',
     short_name: 'JK Rowling Bot',
     lang: 'en',
-    theme_color: '#ebebeb',
-    background_color: '#ebebeb',
+    theme_color: '#3b94d9',
+    background_color: '#3b94d9',
     description: 'JK Rowling Bot',
     display: 'standalone',
     nativeUI: true
